65a847344a8aa6f35a72ed637b85be93f6e6498d
Evan Flynn <evanflynn.msu@gmail.com>
2020-09-16 03:31:03 +0000

    begin migration to ros2

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'ros2serial_msgs'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Evan Flynn',
+    maintainer_email='evanflynn.msu@gmail.com',
+    description='ROS2 port of rosserial package',
+    license='BSD',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
